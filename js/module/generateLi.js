d4c59de3b11cdf63bd0ff58b013558b9d92188c9
llav3ji2019 <pavelemelyanov03@mail.ru>
2022-11-27 02:33:08 +0300

    deleteImage from creatingLi

--- js/module/generateLi.js
+++ js/module/generateLi.js
@@ -27,7 +27,7 @@
     const checkboxReplica  = document.createElement('span');
     checkboxReplica.className = 'item-in-list__checkbox-replica';
 
-    let deleteBtn  = document.createElement('button');
+    const deleteBtn  = document.createElement('button');
     deleteBtn.className = 'item-in-list__delete_btn';
     deleteBtn.role = 'button';
     deleteBtn.ariaLabel = 'Delete this task';
@@ -38,15 +38,11 @@
     };
 
     deleteBtn.addEventListener('click', deleteTask);
-    
-    let deleteImage  = document.createElement('img');
-    deleteImage.alt = 'Изображение удаления';
-    deleteImage.src = '../images/delete.svg';
 
-    let editDiv  = document.createElement('div');
+    const editDiv  = document.createElement('div');
     editDiv.className = 'item-in-list__edit';
 
-    let editInput  = document.createElement('input');
+    const editInput  = document.createElement('input');
     editInput.id = task.id;
     editInput.type = 'text';
     editInput.className = 'task-item__input';
@@ -54,7 +50,6 @@
     editInput.value = task.desc;
 
     customCheckbox.append(inputWithMark, checkboxReplica);
-    deleteBtn.appendChild(deleteImage);
     taskItem.append(customCheckbox, mainListText, deleteBtn);
     editDiv.appendChild(editInput);
     itemInList.append(taskItem, editDiv);
